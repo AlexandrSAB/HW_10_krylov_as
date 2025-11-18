dc88e120d0ea64c6cfbdfd5e863f6428b6edbf95
Alexandr Krylov <b500201@yandex.ru>
2025-11-18 18:31:53 +0300

    Готово!

--- app/src/main/java/com/example/hw_10_krylov_as/MainActivity.java
+++ app/src/main/java/com/example/hw_10_krylov_as/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.hw_10_krylov_as;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Constants{
+
+    private EditText txtName;
+    private EditText txtAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initView();
+    }
+
+    private void initView() {
+        txtName = findViewById(R.id.textName);
+        txtAge = findViewById(R.id.textAge);
+        Button btnCongratulations = findViewById(R.id.btnCongratulations);
+
+        btnCongratulations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent runCongratulations = new Intent(MainActivity.this, CongratulationsActivity.class);
+                runCongratulations.putExtra(YOUR_NAME, (" " + txtName.getText().toString()));
+                runCongratulations.putExtra(YOUR_AGE, (" " + txtAge.getText().toString())+ " ");
+                startActivity(runCongratulations);
+            }
+        });
     }
 }
+
